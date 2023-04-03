d81f27b85225401e21de99b8507168b1b42ef103
faisalamin001 <faysalamin001@gmail.com>
2023-04-03 11:52:21 +0500

    gradient design add

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,12 +1,15 @@
 const Hero = () => {
   return (
-    <div className='text-center py-32 bg-gradient-to-r from-slate-900 to-slate-700 text-white'>
-      <h1 className=' font-bold text-8xl capitalize'>
-        Put Your Skills
+    <div className='text-center py-24 bg-gradient-to-r from-slate-900 to-slate-700 text-white'>
+      <h1 className=' font-bold text-9xl capitalize'>
+        Put Your{' '}
+        <span className='bg-gradient-to-r from-pink-600 to-blue-600 bg-clip-text text-transparent'>
+          Skills
+        </span>
         <br />
         To The Test!
       </h1>
-      <button className='mt-16 text-white bg-gray-900 hover:border  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 '>
+      <button className='mt-16 text-white bg-gray-900 hover:border  font-medium rounded text-sm px-5 py-2.5 mr-2 mb-2 '>
         Take the Quiz
       </button>
     </div>

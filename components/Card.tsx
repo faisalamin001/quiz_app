7d9d696fbf77686495738c8ffe8e2e9aec9d9d20
faisalamin001 <faysalamin001@gmail.com>
2023-04-06 15:12:17 +0500

    class changed to className

--- components/Card.tsx
+++ components/Card.tsx
@@ -39,41 +39,41 @@ const Card = ({ questions }: Props) => {
       {questions && questions.length < 1 && (
         <div>
           {' '}
-          <div class=' shadow  p-4 rounded-xl bg-slate-900 max-w-sm h-96	 w-64 border border-slate-200  mx-auto'>
-            <div class='animate-pulse'>
-              <div class='flex-1 space-y-6 py-1'>
-                <div class='h-2 bg-slate-700 rounded'></div>
-                <div class='space-y-3'>
-                  <div class='grid grid-cols-3 gap-4'>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
+          <div className=' shadow  p-4 rounded-xl bg-slate-900 max-w-sm h-96	 w-64 border border-slate-200  mx-auto'>
+            <div className='animate-pulse'>
+              <div className='flex-1 space-y-6 py-1'>
+                <div className='h-2 bg-slate-700 rounded'></div>
+                <div className='space-y-3'>
+                  <div className='grid grid-cols-3 gap-4'>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
                   </div>
-                  <div class='h-2 bg-slate-700 rounded'></div>
-                  <div class='grid grid-cols-3 gap-4'>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+                  <div className='h-2 bg-slate-700 rounded'></div>
+                  <div className='grid grid-cols-3 gap-4'>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
                   </div>
                 </div>
-                <div class='h-2 bg-slate-700 rounded'></div>
+                <div className='h-2 bg-slate-700 rounded'></div>
               </div>
-              <div class='flex-1 space-y-6 py-1'>
-                <div class='h-2 bg-slate-700 rounded'></div>
-                <div class='space-y-3'>
-                  <div class='grid grid-cols-3 gap-4'>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
+              <div className='flex-1 space-y-6 py-1'>
+                <div className='h-2 bg-slate-700 rounded'></div>
+                <div className='space-y-3'>
+                  <div className='grid grid-cols-3 gap-4'>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
                   </div>
-                  <div class='h-2 bg-slate-700 rounded'></div>
-                  <div class='grid grid-cols-3 gap-4'>
-                    <div class='h-2 bg-slate-700 rounded col-span-2'></div>
-                    <div class='h-2 bg-slate-700 rounded col-span-1'></div>
+                  <div className='h-2 bg-slate-700 rounded'></div>
+                  <div className='grid grid-cols-3 gap-4'>
+                    <div className='h-2 bg-slate-700 rounded col-span-2'></div>
+                    <div className='h-2 bg-slate-700 rounded col-span-1'></div>
                   </div>
                 </div>
-                <div class='h-2 bg-slate-700 rounded'></div>
+                <div className='h-2 bg-slate-700 rounded'></div>
               </div>
             </div>
           </div>
